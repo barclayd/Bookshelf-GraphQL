2ff2372c247cb772ad8bf5e39e7318ea50a377b5
Daniel Barclay <daniel@urbanintel.io>
2018-12-02 00:04:00 +0000

    GraphiQL for testing queries

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,15 @@
+const _ = require('lodash');
 const graphql = require('graphql');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// mock data
+
+const books = [
+    {name: 'Gone with the Wind', genre: 'Drama', id: '1'},
+    {name: 'Empire Strikes Back', genre: 'Fantasy', id: '2'},
+    {name: 'Everything there is to know about India', genre: 'Travel', id: '3'}
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -16,12 +25,10 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {
-                id: {
-                    type: GraphQLString
-                }},
+            args: {id: {type: GraphQLString}},
             resolve(parent, args) {
                 // code to retrieve data from MongoDB
+                return _.find(books, {id: args.id});
             }
         }
     }
