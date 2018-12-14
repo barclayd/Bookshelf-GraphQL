d6640324ca043930467bbd0e6f7d88ccea47fb23
Daniel Barclay <daniel@urbanintel.io>
2018-12-14 22:32:50 +0000

    NonNull for mutation validation

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,10 @@ const { GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
     GraphQLID,
-    GraphQLInt, GraphQLList} = graphql;
+    GraphQLInt,
+    GraphQLList,
+    GraphQLNonNull
+} = graphql;
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -46,8 +49,8 @@ const Mutation = new GraphQLObjectType({
         addAuthor: {
             type: AuthorType,
             args: {
-                name: {type: GraphQLString},
-                age: {type: GraphQLInt}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                age: {type: new GraphQLNonNull(GraphQLInt)}
             },
             resolve(parent, args) {
                 let author = new Author({
@@ -60,9 +63,9 @@ const Mutation = new GraphQLObjectType({
         addBook: {
             type: BookType,
             args: {
-                name: {type: GraphQLString},
-                genre: {type: GraphQLString},
-                authorId: {type: GraphQLID}
+                name: {type: new GraphQLNonNull(GraphQLString)},
+                genre: {type: new GraphQLNonNull(GraphQLString)},
+                authorId: {type: new GraphQLNonNull(GraphQLID)}
             },
             resolve(parent, args) {
                 let book = new Book({
