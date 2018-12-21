19e2e1f70c580cee53c8ead1f3402ea47c999e23
Daniel Barclay <barclaysd@me.com>
2018-12-21 23:52:06 +0000

    AddBook using dynamic query variables and refetch query onSubmit

--- client/src/queries/queries.js
+++ client/src/queries/queries.js
@@ -18,6 +18,16 @@ const getBooksQuery = gql`
     }
 `;
 
+
+const addBookMutation = gql`
+    mutation($name: String!, $genre: String!, $authorId: ID! ) {
+        addBook(name: $name, genre: $genre, authorId: $authorId) {
+            name
+            id
+        }
+    }
+`;
+
 export {
-    getAuthorsQuery, getBooksQuery
+    getAuthorsQuery, getBooksQuery, addBookMutation
 };
