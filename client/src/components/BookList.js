3dec9c6b4df2d32b51e6231152783ef9c88e6e3b
Daniel Barclay <daniel@urbanintel.io>
2018-12-21 21:34:41 +0000

    Setup ApolloClient and Provider on frontend

---
+++ client/src/components/BookList.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+class BookList extends Component {
+
+    render () {
+
+        return (
+            <React.Fragment>
+                <ul id='book-list'>
+                    <li>Book Name</li>
+                </ul>
+            </React.Fragment>
+        )
+    }
+}
+
+export default BookList;
