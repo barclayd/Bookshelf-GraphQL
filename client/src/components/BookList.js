1372de8ac073139526947ccea07821c384ef388d
Daniel Barclay <daniel@urbanintel.io>
2018-12-21 22:03:01 +0000

    Rendered books from MongoDB, updated font and added a loading spinner

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,6 +1,7 @@
-import React, {Component} from "react";
+import React from "react";
 import {gql} from 'apollo-boost';
 import {graphql} from 'react-apollo';
+import Spinner from './UI/Spinner/Spinner';
 
 const getBooksQuery = gql`
     {
@@ -11,18 +12,28 @@ const getBooksQuery = gql`
     }
 `;
 
-class BookList extends Component {
+const BookList = props => {
 
-    render () {
-        console.log(this.props);
+    const displayBooks = () => {
+        const data = props.data;
+        if(data.loading){
+            return <Spinner/>
+        } else {
+            return data.books.map((book) => {
+                return (<li key={book.id}>{book.name}</li>
+                );
+            });
+        }
+    };
+
+    console.log(props.data);
         return (
             <React.Fragment>
                 <ul id='book-list'>
-                    <li>Book Name</li>
+                    {displayBooks()}
                 </ul>
             </React.Fragment>
         )
-    }
-}
+};
 
 export default graphql(getBooksQuery)(BookList);
---
+++ client/src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './Spinner.module.css';
+
+const spinner = () => (
+    <div className={classes["lds-spinner"]}>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+);
+
+export default spinner;
+
